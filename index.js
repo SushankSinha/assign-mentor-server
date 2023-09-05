3fe5639db2184f50a6b867d9b0041902cef3d9cc
SushankSinha <SushankSinha>
2023-09-05 21:07:34 +0530

    url update

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.listen(PORT, () => console.log("Server starting on port", PORT));
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["https://assign-mentor-client.netlify.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
